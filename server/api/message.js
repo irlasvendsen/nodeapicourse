31825351c216cffdd35bf9cf76e461aa8b921f5d
Irla Svendsen <irlasvendsen@gmail.com>
2024-02-29 12:09:09 -0500

    add test console (#2)

--- server/api/message.js
+++ server/api/message.js
@@ -5,6 +5,8 @@ exports.messageOfTheDay = async function () {
     console.log('access message of the day')
     const response = await axios.get("https://type.fit/api/quotes")
     const randomNumber = Math.floor(Math.random() * response.data.length) + 1 ;
+	console.log('test');
+	console.log('anothertest');
     //console.log(randomNumber)
     const phrase = `${response.data[randomNumber].text} By ${response.data[randomNumber].author || 'Anonymous'}`;
     //console.log('this is the response->',phrase);
